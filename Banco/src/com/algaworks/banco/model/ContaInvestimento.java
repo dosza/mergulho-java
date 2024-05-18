f66354ad5ee02d661abc07068e9b746413d56e03
dosza <oliveira.daniel109@gmail.com>
2024-05-18 16:40:44 -0300

    implementa ContaInvestimento

--- Banco/src/com/algaworks/banco/model/ContaInvestimento.java
+++ Banco/src/com/algaworks/banco/model/ContaInvestimento.java
@@ -1,5 +1,13 @@
 package com.algaworks.banco.model;
 
 public class ContaInvestimento  extends  Conta{
-    
+    public ContaInvestimento(Pessoa titular, int agencia, int conta){
+        super(titular,agencia,conta);
+    }
+
+    public void creditarRendimentos(double percentualJuros){
+        double valorRedimento = getSaldo() + percentualJuros/100.00;
+        depositar(valorRedimento);
+
+    }
 }
