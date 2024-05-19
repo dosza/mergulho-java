4d8b947c2f89092cfd3aacf65b3e462698928708
dosza <oliveira.daniel109@gmail.com>
2024-05-19 12:35:22 -0300

    add classs TestCalculo

---
+++ Banco/src/com/algaworks/banco/app/TesteCalculo.java
@@ -0,0 +1,12 @@
+package com.algaworks.banco.app;
+
+public class TesteCalculo {
+
+    public static void main(String[] args) {
+        double x = 1.0;
+        x = x -0.1;
+        x = x- 0.2;
+        x = x - 0.3;
+        System.out.println(x);
+    }
+}
