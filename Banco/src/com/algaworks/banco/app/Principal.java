0e36c448315ee6a2f6ce7960ac7dad2b808a88d5
dosza <oliveira.daniel109@gmail.com>
2024-05-19 07:00:15 -0300

    rename object

--- Banco/src/com/algaworks/banco/app/Principal.java
+++ Banco/src/com/algaworks/banco/app/Principal.java
@@ -3,22 +3,23 @@ package com.algaworks.banco.app;
 import com.algaworks.banco.model.*;
 import com.algaworks.banco.model.atm.CaixaEletronico;
 import com.algaworks.banco.model.pagamento.Boleto;
+import com.algaworks.banco.model.pagamento.Holerite;
 
 public class Principal {
     public static void main(String[] args) {
-        Pessoa maria= new Pessoa();
-        Pessoa joao = new Pessoa();
-        joao.setNome( "Joao da Silva");
-        joao.setDocumento ("00000");
-        maria.setNome("Maria Aparecida");
-        maria.setDocumento("11110");
+        Pessoa titular= new Pessoa();
+        Pessoa titular2 = new Pessoa();
+        titular2.setNome( "Joao da Silva");
+        titular2.setDocumento ("00000");
+        titular.setNome("Maria Aparecida");
+        titular.setDocumento("11110");
         CaixaEletronico caixaEletronico = new CaixaEletronico();
 
 
-        ContaInvestimento minhaConta = new ContaInvestimento(joao,1,1);
-        minhaConta.depositar(15000);
+        ContaInvestimento minhaConta = new ContaInvestimento(titular2,1,1);
+        minhaConta.depositar(30000);
        // minhaConta.creditarRendimentos(0.8);
-        ContaEspecial suaConta = new ContaEspecial(maria,1,2,1000);
+        ContaEspecial suaConta = new ContaEspecial(titular,1,2,1000);
 
         suaConta.depositar(15_000);
         suaConta.sacar(15_500);
@@ -26,10 +27,16 @@ public class Principal {
        // minhaConta.debitarTarifaMensal();
         suaConta.debitarTarifaMensal();
 
-        Boleto boletoEscola  = new Boleto(joao,800);
-        System.out.println("Boleto pago: " + boletoEscola.estáPago());
+        Boleto boletoEscola  = new Boleto(titular2,800);
+        Holerite salarioFuncionario = new Holerite(titular2,100,160);
+
+
         caixaEletronico.pagar(boletoEscola,minhaConta);
+        caixaEletronico.pagar(salarioFuncionario,minhaConta);
         System.out.println("Boleto pago: " + boletoEscola.estáPago());
+        System.out.println("Salário pago: " + salarioFuncionario.estáPago());
+
+
 
         System.out.println();
         caixaEletronico.imprimirSaldo(minhaConta);
