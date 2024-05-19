c09f7de5e6ae703489a2e417965aa2894400b297
dosza <oliveira.daniel109@gmail.com>
2024-05-19 20:54:23 -0300

    add class Principal2

---
+++ Banco/src/com/algaworks/banco/app/Principal2.java
@@ -0,0 +1,10 @@
+package com.algaworks.banco.app;
+
+import com.algaworks.banco.model.Pessoa;
+
+public class Principal2 {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa("João da Silva,","12312312311");
+        System.out.println(pessoa1.toString());
+    }
+}
