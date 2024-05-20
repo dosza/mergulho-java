09ab7c25924eedde3f0d9be8f14dd042322e6fdb
dosza <oliveira.daniel109@gmail.com>
2024-05-19 22:11:27 -0300

    use NumeroContaFunction to sort contas

--- Banco/src/com/algaworks/banco/app/Principal3.java
+++ Banco/src/com/algaworks/banco/app/Principal3.java
@@ -3,10 +3,12 @@ package com.algaworks.banco.app;
 import com.algaworks.banco.model.Banco;
 import com.algaworks.banco.model.Conta;
 
+import java.util.Comparator;
+
 public class Principal3 {
     public static void main(String[] args) {
         Banco banco = new Banco();
-
+        banco.getContas().sort(Comparator.comparingInt(new NumeroContaFunction()));
         for (Conta conta: banco.getContas()){
             System.out.println(conta.getAgencia() + "/" + conta.getNumero());
         }
