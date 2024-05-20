c9bf332dee5d1ce6e45e4ac900daac4a3cddd45a
dosza <oliveira.daniel109@gmail.com>
2024-05-19 21:45:29 -0300

    add class Principal3

---
+++ Banco/src/com/algaworks/banco/app/Principal3.java
@@ -0,0 +1,14 @@
+package com.algaworks.banco.app;
+
+import com.algaworks.banco.model.Banco;
+import com.algaworks.banco.model.Conta;
+
+public class Principal3 {
+    public static void main(String[] args) {
+        Banco banco = new Banco();
+
+        for (Conta conta: banco.getContas()){
+            System.out.println(conta.getAgencia() + "/" + conta.getNumero());
+        }
+    }
+}
