ab698bb6c3b0269f6505ed8b5eea61cc81b16ba0
dosza <oliveira.daniel109@gmail.com>
2024-05-19 22:18:14 -0300

    use lambda function

--- Banco/src/com/algaworks/banco/app/Principal3.java
+++ Banco/src/com/algaworks/banco/app/Principal3.java
@@ -4,11 +4,16 @@ import com.algaworks.banco.model.Banco;
 import com.algaworks.banco.model.Conta;
 
 import java.util.Comparator;
+import java.util.function.ToIntFunction;
 
 public class Principal3 {
     public static void main(String[] args) {
         Banco banco = new Banco();
-        banco.getContas().sort(Comparator.comparingInt(new NumeroContaFunction()));
+        //ToIntFunction as lambda function
+        ToIntFunction<Conta> function = (Conta conta) -> {
+          return conta.getNumero();
+        };
+        banco.getContas().sort(Comparator.comparingInt(function));
         for (Conta conta: banco.getContas()){
             System.out.println(conta.getAgencia() + "/" + conta.getNumero());
         }
