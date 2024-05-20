213742c29f7712c8f0c063d198d3f76bd6022bdd
dosza <oliveira.daniel109@gmail.com>
2024-05-19 22:20:00 -0300

    use lambda function as argument

--- Banco/src/com/algaworks/banco/app/Principal3.java
+++ Banco/src/com/algaworks/banco/app/Principal3.java
@@ -9,11 +9,9 @@ import java.util.function.ToIntFunction;
 public class Principal3 {
     public static void main(String[] args) {
         Banco banco = new Banco();
-        //ToIntFunction as lambda function
-        ToIntFunction<Conta> function = (Conta conta) -> {
-          return conta.getNumero();
-        };
-        banco.getContas().sort(Comparator.comparingInt(function));
+
+        banco.getContas().sort(Comparator.comparingInt(
+                (Conta conta)-> conta.getNumero()));
         for (Conta conta: banco.getContas()){
             System.out.println(conta.getAgencia() + "/" + conta.getNumero());
         }
