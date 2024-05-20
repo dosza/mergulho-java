b8ebd7675f0e405d419d6cdd27c377a760249d3a
dosza <oliveira.daniel109@gmail.com>
2024-05-19 23:06:08 -0300

    use collection get  a List<Pessoa>

--- Banco/src/com/algaworks/banco/app/Principal4.java
+++ Banco/src/com/algaworks/banco/app/Principal4.java
@@ -2,19 +2,21 @@ package com.algaworks.banco.app;
 
 import com.algaworks.banco.model.Banco;
 import com.algaworks.banco.model.Conta;
+import com.algaworks.banco.model.Pessoa;
 
 import java.math.BigDecimal;
 import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Principal4 {
     public static void main(String[] args) {
         Banco banco = new Banco();
-        BigDecimal saldoTotal = banco.getContas().stream()
-                .map(Conta::getSaldo)
-                .reduce(BigDecimal.ZERO,  BigDecimal::add);
-
-        System.out.println(saldoTotal);
+        //usa collection dentro de stream para obter uma lista  de pessoas
+        List<Pessoa> pessas  = banco.getContas().stream()
+                .map(Conta::getTitular)
+                .distinct()
+                .toList();
 
     }
 }
