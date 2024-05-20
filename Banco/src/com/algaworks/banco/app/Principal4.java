3bba9951968260dd657e6cda9e71a81ae56e7ffe
dosza <oliveira.daniel109@gmail.com>
2024-05-19 22:48:29 -0300

    do stream in pipes

--- Banco/src/com/algaworks/banco/app/Principal4.java
+++ Banco/src/com/algaworks/banco/app/Principal4.java
@@ -9,16 +9,14 @@ import java.util.stream.Stream;
 public class Principal4 {
     public static void main(String[] args) {
         Banco banco = new Banco();
-        Stream<Conta> stream = banco.getContas().stream();
-        Stream<Conta> stream2 = stream.filter(
-                conta   ->  conta.getSaldo().compareTo(new BigDecimal("130")) > 0 );
-
-        Stream<Conta> stream3 = stream2.filter( conta-> conta.getNumero() > 300);
-
-        stream3.forEach(conta->{
-            System.out.println(
-                    conta.getAgencia()+ "/"+conta.getNumero()
-                    + " = " + conta.getSaldo()) ;
-        });
+        banco.getContas().stream()
+                .filter(
+                        conta -> conta.getSaldo().compareTo(new BigDecimal("130")) > 0)
+                .filter(conta -> conta.getNumero() > 300)
+                .forEach(conta -> {
+                    System.out.println(
+                            conta.getAgencia() + "/" + conta.getNumero()
+                                    + " = " + conta.getSaldo());
+                });
     }
 }
