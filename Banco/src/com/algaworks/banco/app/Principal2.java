64ebe406b6c602a85e7dec96b8eb4487d4ce8e55
dosza <oliveira.daniel109@gmail.com>
2024-05-19 21:19:32 -0300

    use a HashSet

--- Banco/src/com/algaworks/banco/app/Principal2.java
+++ Banco/src/com/algaworks/banco/app/Principal2.java
@@ -3,13 +3,16 @@ package com.algaworks.banco.app;
 import com.algaworks.banco.model.Pessoa;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Principal2 {
     public static void main(String[] args) {
         Pessoa pessoa1 = new Pessoa("João da Silva,", "12312312311");
         Pessoa pessoa2 = new Pessoa("Maria das Couves", "22222222111");
-        List<Pessoa> pessoas = new ArrayList<>();
+        //List<Pessoa> pessoas = new ArrayList<>();
+        Set<Pessoa> pessoas = new HashSet();
 
         pessoas.add(pessoa1);
         pessoas.add(pessoa2);
@@ -21,15 +24,15 @@ public class Principal2 {
       //  for (int i = 0;i <pessoas.size();i++){
       //      System.out.println(pessoas.get(i).getNome());
        // }
-        for (Pessoa pessoa : pessoas){
-            System.out.println(pessoa.getNome());
-        }
+       // for (Pessoa pessoa : pessoas){
+       //     System.out.println(pessoa.getNome());
+       // }
 
         Pessoa pessoa4 = new Pessoa("João da Silva,", "12312312311");
 
         boolean  existe = pessoas.contains(pessoa4);
         System.out.println(existe);
-        System.out.println(pessoa1.equals(pessoa4));
+      //  System.out.println(pessoa1.equals(pessoa4));
 
     }
 }
