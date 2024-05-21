2232307761e334480d560a97ee1440fdc318b673
dosza <oliveira.daniel109@gmail.com>
2024-05-20 22:21:05 -0300

    use Optionalt get conta or launch exception

--- Banco/src/com/algaworks/banco/app/Principal5.java
+++ Banco/src/com/algaworks/banco/app/Principal5.java
@@ -8,7 +8,11 @@ import java.util.Optional;
 public class Principal5 {
     public static void main(String[] args) {
         Banco banco =new Banco();
-        //use optional com expressao lambda
-        banco.buscar(123,333).ifPresent(Conta::getSaldo);
+
+        //Obtem a conta em contrada ou lança exceção com Optional
+        Conta contaEncontrada=banco.buscar(123,333)
+                .orElseThrow( ()-> new RuntimeException( "Conta não encontrada"));
+
+        System.out.println(contaEncontrada.getTitular());
     }
 }
