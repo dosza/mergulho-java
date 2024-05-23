f196e980eb7be95d274cad4a93767ce2ed5f88e6
dosza <oliveira.daniel109@gmail.com>
2024-05-23 19:16:06 -0300

    add equals,hashcode and to String in Funcionario

--- rh-api/src/main/java/com/algaworks/rh/rhapi/model/Funcionario.java
+++ rh-api/src/main/java/com/algaworks/rh/rhapi/model/Funcionario.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Id;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 public class Funcionario {
@@ -16,6 +17,78 @@ public class Funcionario {
     private String email;
     private BigDecimal salario;
     private LocalDateTime dataAdmissao;
+    private StatusFuncionario status;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Funcionario that = (Funcionario) o;
+        return id == that.id && Objects.equals(nome, that.nome) && Objects.equals(email, that.email) && Objects.equals(salario, that.salario) && Objects.equals(dataAdmissao, that.dataAdmissao) && status == that.status;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, email, salario, dataAdmissao, status);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public LocalDateTime getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(LocalDateTime dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public StatusFuncionario getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusFuncionario status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", salario=" + salario +
+                ", dataAdmissao=" + dataAdmissao +
+                ", status=" + status +
+                '}';
+    }
 }
