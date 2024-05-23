02c62ec6d71f1926d2a5a9a17a6f12e7af656c37
dosza <oliveira.daniel109@gmail.com>
2024-05-23 19:27:24 -0300

    add FuncionarioRepository

--- rh-api/src/main/java/com/algaworks/rh/rhapi/repository/FuncionarioRepository.java
+++ rh-api/src/main/java/com/algaworks/rh/rhapi/repository/FuncionarioRepository.java
@@ -1,2 +1,8 @@
-package com.algaworks.rh.rhapi.repository;public interface FuncionarioRepository {
+package com.algaworks.rh.rhapi.repository;
+
+import com.algaworks.rh.rhapi.model.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FuncionarioRepository  extends JpaRepository<Funcionario,Long> {
+
 }
