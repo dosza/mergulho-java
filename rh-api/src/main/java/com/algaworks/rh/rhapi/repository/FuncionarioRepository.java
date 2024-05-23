52321f2393b55f69e1bbd09a7882a49a0e67f4d1
dosza <oliveira.daniel109@gmail.com>
2024-05-23 19:28:53 -0300

    adiciona anotaçao @Repository que implementa a interface FuncionarioRepository

--- rh-api/src/main/java/com/algaworks/rh/rhapi/repository/FuncionarioRepository.java
+++ rh-api/src/main/java/com/algaworks/rh/rhapi/repository/FuncionarioRepository.java
@@ -2,7 +2,9 @@ package com.algaworks.rh.rhapi.repository;
 
 import com.algaworks.rh.rhapi.model.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FuncionarioRepository  extends JpaRepository<Funcionario,Long> {
 
 }
