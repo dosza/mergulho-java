7d7998c79222f1d716ddda3311ff4b62ece8a907
dosza <oliveira.daniel109@gmail.com>
2024-05-23 22:59:24 -0300

    substitue LocalDateTime para LocalDate e substui getters,setters pela anotacao @Getteer @Setter

--- rh-api/src/main/java/com/algaworks/rh/rhapi/model/Funcionario.java
+++ rh-api/src/main/java/com/algaworks/rh/rhapi/model/Funcionario.java
@@ -1,13 +1,20 @@
 package com.algaworks.rh.rhapi.model;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.Objects;
+import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Funcionario {
 
     @Id
@@ -17,69 +24,8 @@ public class Funcionario {
     private String nome;
     private String email;
     private BigDecimal salario;
-    private LocalDateTime dataAdmissao;
+    private LocalDate dataAdmissao;
     private StatusFuncionario status;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Funcionario that = (Funcionario) o;
-        return id == that.id && Objects.equals(nome, that.nome) && Objects.equals(email, that.email) && Objects.equals(salario, that.salario) && Objects.equals(dataAdmissao, that.dataAdmissao) && status == that.status;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, nome, email, salario, dataAdmissao, status);
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public BigDecimal getSalario() {
-        return salario;
-    }
-
-    public void setSalario(BigDecimal salario) {
-        this.salario = salario;
-    }
-
-    public LocalDateTime getDataAdmissao() {
-        return dataAdmissao;
-    }
-
-    public void setDataAdmissao(LocalDateTime dataAdmissao) {
-        this.dataAdmissao = dataAdmissao;
-    }
-
-    public StatusFuncionario getStatus() {
-        return status;
-    }
-
-    public void setStatus(StatusFuncionario status) {
-        this.status = status;
-    }
 
     @Override
     public String toString() {
